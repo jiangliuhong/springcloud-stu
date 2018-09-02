dcf8b3023e5881a7fa977cce89f19a6d5ff121ac
jarome <ja_rome@163.com>
2018-09-02 12:05:34 +0800

    add client

---
+++ scs-eureka-client/src/main/java/pers/jarome/scs/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,20 @@
+package pers.jarome.scs.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ *
+ * EurekaClientApplication
+ * @author jiangliuhong
+ * @date 2018/9/2 11:52
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
