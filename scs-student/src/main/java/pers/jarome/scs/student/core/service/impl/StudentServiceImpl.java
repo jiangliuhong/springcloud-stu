1545f80c22ea251573b495b222280c86fd7abe79
jarome <ja_rome@163.com>
2018-09-02 17:28:35 +0800

    add trans

--- scs-student/src/main/java/pers/jarome/scs/student/core/service/impl/StudentServiceImpl.java
+++ scs-student/src/main/java/pers/jarome/scs/student/core/service/impl/StudentServiceImpl.java
@@ -2,6 +2,7 @@ package pers.jarome.scs.student.core.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pers.jarome.scs.student.core.dao.StudentDao;
 import pers.jarome.scs.student.core.model.Student;
 import pers.jarome.scs.student.core.service.StudentService;
@@ -13,6 +14,7 @@ public class StudentServiceImpl implements StudentService {
     private StudentDao studentDao;
 
     @Override
+    @Transactional
     public void addStudent(Student student) {
         studentDao.insert(student);
     }
