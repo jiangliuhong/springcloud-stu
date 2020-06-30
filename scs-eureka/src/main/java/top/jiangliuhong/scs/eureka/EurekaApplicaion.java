da321a56be3bd16a8d6daad94297c079f43820d1
jiangliuhong <jerome950523@gmail.com>
2020-06-30 23:39:21 +0800

    init scs eureka

---
+++ scs-eureka/src/main/java/top/jiangliuhong/scs/eureka/EurekaApplicaion.java
@@ -0,0 +1,20 @@
+package top.jiangliuhong.scs.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * EurekaApplicaion
+ * @description 启动类
+ * @author jiangliuhong
+ * @date 2020/6/30 11:37 下午
+ * @version 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplicaion {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplicaion.class, args);
+    }
+}
