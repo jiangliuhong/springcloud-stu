8d35b5163f3d65f9e8f48ec099fa44cb0776497d
jiangliuhong <jerome950523@gmail.com>
2020-08-23 00:38:53 +0800

    增加starter-redis

---
+++ scs-components/scs-starter-redis/src/main/java/top/jiangliuhong/scs/redis/properties/CacheManagerProperties.java
@@ -0,0 +1,30 @@
+package top.jiangliuhong.scs.redis.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+/**
+ * 缓存配置
+ */
+@Setter
+@Getter
+@ConfigurationProperties(prefix = "scs.cache-manager")
+public class CacheManagerProperties {
+    private List<CacheConfig> configs;
+
+    @Setter
+    @Getter
+    public static class CacheConfig {
+        /**
+         * cache key
+         */
+        private String key;
+        /**
+         * 过期时间，sec
+         */
+        private long second = 60;
+    }
+}
